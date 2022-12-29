4c8afe104cac77450f1c5bfed5fa9794477b48a3
Scaliariy <scaliariy1@gmail.com>
2022-12-29 11:41:59 +0200

    test classes

---
+++ CalculateDiscountTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculateDiscountTest {
+
+
+    @ParameterizedTest
+    @CsvSource({"1,0", "2,0"})
+    void typeRegularTest(int number, int discount) {
+        int actualDiscount = ShoppingCart.calculateDiscount(ShoppingCart.ItemType.NEW, number);
+        assertEquals(discount, actualDiscount);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,0", "2,50"})
+    void typeSecondTest(int number, int discount) {
+        int actualDiscount = ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, number);
+        assertEquals(discount, actualDiscount);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,0", "10,1", "20,2", "800,80", "900,80"})
+    void typeDiscountTest(int number, int discount) {
+        int actualDiscount = ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, number);
+        assertEquals(discount, actualDiscount);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,70", "2,70", "20,72", "100,80", "200,80"})
+    void typeSaleTest(int number, int discount) {
+        int actualDiscount = ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, number);
+        assertEquals(discount, actualDiscount);
+    }
+}
